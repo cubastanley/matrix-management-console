37dcde930a0739dae837cf194b5cb41724552b9a
Cuba Stanley <cubastanley@rowdentech.com>
2023-09-11 11:33:36 +0100

    Created data util file

---
+++ lib/data.ts
@@ -0,0 +1,14 @@
+export const links = [
+    {
+        name: "User Management",
+        slug: "user-management"
+    },
+    {
+        name: "Room Management",
+        slug: "room-management"
+    },
+    {
+        name: "Server Configuration",
+        slug: "settings"
+    },
+] as const;
